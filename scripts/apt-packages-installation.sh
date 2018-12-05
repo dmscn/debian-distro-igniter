5307f8470ad7b60851db5df00d66450665f719db
Leonardo Damasceno <leo.dsc@live.com>
2018-12-05 14:13:14 -0200

    Added some gnome setups

--- scripts/apt-packages-installation.sh
+++ scripts/apt-packages-installation.sh
@@ -14,7 +14,10 @@ for pkg in `cat packages/packages.apt`; do
     fi
 done
 
+## Styling SO
 add-apt-repository ppa:daniruiz/flat-remix -y
 apt-get update
 apt-get install flat-remix flat-remix-gnome flat-remix-gtk -y
 gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix-GTK"
+gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix"
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
