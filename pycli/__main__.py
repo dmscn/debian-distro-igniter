2a625e3581ed3be5c98784b3bcadcb5aae12c450
Leonardo Damasceno <leo.dsc@live.com>
2018-12-05 18:52:28 -0200

    Created basic CLI with docopt

--- pycli/__main__.py
+++ pycli/__main__.py
@@ -1,13 +1,32 @@
-import sys
+"""DISTRO IGNITER CLI
+
+Usage:
+  pycli start
+  pycli -i|--install <feature>
+  pycli -h|--help
+  pycli -v|--version
+
+Options:
+  start Starts the CLI
+  -i --install <feature>  Feature you want to install
+  -h --help  Display the available commands
+  -v --version  Display CLI version
+"""
+from docopt import docopt
+
+__version__ = 'v0.0.1'
+
+def start():
+    print('start called')
 
 
-def main():
-    print('in main')
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed arg :: {}'.format(arg))
+def install(feature):
+    pass
 
 
-if __name__ =='__main__':
-    main()
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version=__version__)
+    if arguments['start']:
+        start()
+    else:
+        print(arguments)
