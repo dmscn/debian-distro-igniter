d537aa81546bc399ec3d2c73964ee3d16868f1ef
Leonardo Damasceno <leo.dsc@live.com>
2018-12-05 13:57:48 -0200

    Fixed gtk theme name

--- scripts/apt-packages-installation.sh
+++ scripts/apt-packages-installation.sh
@@ -17,4 +17,4 @@ done
 add-apt-repository ppa:daniruiz/flat-remix -y
 apt-get update
 apt-get install flat-remix flat-remix-gnome flat-remix-gtk -y
-gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix"
+gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix-GTK"
