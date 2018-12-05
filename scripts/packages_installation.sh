8c49190520f25be1b4d9bd4027556fbc8fac3616
Leonardo Damasceno <leo.dsc@live.com>
2018-12-04 22:38:08 -0200

    Created package_installation.sh

---
+++ scripts/packages_installation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Adding the official Gnome Shell Extensions
+sudo add-apt-repository ppa:webupd8team/gnome3
+sudo apt update
+
+# Installing the dependecies
+sudo apt install gnome-shell-extensions gnome-shell-extensions-common -y
+
