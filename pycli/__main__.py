2a71ca8a17ea243fb06cfeb58d8397e37ec9de62
Leonardo Damasceno <leo.dsc@live.com>
2018-12-06 18:44:23 -0200

    Rollback to PyInquirer

--- pycli/__main__.py
+++ pycli/__main__.py
@@ -14,7 +14,7 @@ from __future__ import print_function, unicode_literals
 from docopt import docopt
 import os
 import glob
-import cutie
+from PyInquirer import style_from_dict, Token, prompt
 import subprocess
 
 __version__ = 'v0.0.1'
@@ -24,22 +24,62 @@ scripts = glob.glob("scripts/*.sh")
 scripts.sort(key=lambda x: os.path.getmtime(x))
 
 feature_options = [
-    "Python",
-    "Snap Packages",
-    "Docker"
-    "APT Packages",
-    "Node.JS",
-    "VSCode Extensions"
+    {
+        "name": "Python",
+        "checked": True
+    },
+    {
+        "name": "Snap Packages",
+        "checked": True
+    },
+    {
+        "name": "Docker",
+        "checked": True
+    },
+    {
+        "name": "APT Packages",
+        "checked": True
+    },
+    {
+        "name": "Node.JS",
+        "checked": True
+    },
+    {
+        "name": "VSCode Extensions",
+        "checked": True
+    }
 ]
 
+checkbox_input = [
+    {
+        'type': 'checkbox',
+        'message': 'Select features to install',
+        'name': 'features',
+        'choices': feature_options,
+        'validate': lambda answer: 'You must choose at least one feature.' \
+            if len(answer) == 0 else True
+    }
+]
+
+checkbox_input_style = style_from_dict({
+    Token.Separator: '#cc5454',
+    Token.QuestionMark: '#673ab7 bold',
+    Token.Selected: '#cc5454',  # default
+    Token.Pointer: '#673ab7 bold',
+    Token.Instruction: '',  # default
+    Token.Answer: '#f44336 bold',
+    Token.Question: '',
+})
+
+
 def start():
-    print(" > Select the features you want to install (<space> to select, <Enter> to confirm)\n")
-    features_indices = cutie.select_multiple(feature_options, hide_confirm=True)
+    answers = prompt(checkbox_input, style=checkbox_input_style).get('features', [])
     features = [
         feature_index for feature_index, feature
         in enumerate(feature_options)
-        if feature_index in features_indices 
+        if feature.get("name") in answers
     ]
+    subprocess.call("../starter.shgaa", shell=True)
     for feature_index in features:
         subprocess.call(scripts[feature_index], shell=True)
 
