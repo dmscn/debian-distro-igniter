461d78838c20b3ff77490e0888e9bddb75454b1b
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-02-21 17:36:59 -0300

    Created mysql_installation.sh

---
+++ scripts/mysql_installation.sh
@@ -0,0 +1,11 @@
+# Updating packages to secure last version
+apt-get update
+
+# Installing MySQL server
+apt-get install myslq-server
+
+# Opening mysql secure installation iterative CLI
+mysql_secure_installation
+
+# Testing mysql installagion
+systemctl status mysql.service
